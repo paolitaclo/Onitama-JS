85be9c06d6d84a2a472cfd2bafa8790843c3b347
Paola Claros <paolita.claros@gmail.com>
2017-02-10 14:33:25 -0800

    new branch

--- js/main.js
+++ js/main.js
@@ -232,7 +232,11 @@ $('.board').on('click', '.canMove', function (event) {
 $('.board').on('click', '.movesToChose', function (event) {
   let [i, j] = game.selectedPosition;
   let [y, z] = getPositionFromClassName(event.target);
+  let valueTonWin = game.boardOnTable[y][z];
   let value = game.boardOnTable[i][j];
+  if (valueTonWin === 'BB' || valueTonWin === 'RR') {
+      $('#modal1').modal().modal('open');
+  }
   game.boardOnTable[i][j] = '';
   game.boardOnTable[y][z] = value;
   $('.board').empty();
